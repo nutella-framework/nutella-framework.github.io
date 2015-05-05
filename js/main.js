431e1d75b90ae73ccfe6a543e19f8fa3a2e486ef
Alessandro Gnoli <tebemis@gmail.com>
2015-05-05 09:34:51 -0500

    Updated language and version

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
 
+    // Fetch nutella version from Github as a JSONP request
+    $.ajax({
+        url: "https://api.github.com/repos/nutella-framework/nutella_framework/contents/VERSION",
+        jsonp: "callback",
+        dataType: "jsonp",
+        success: function( response ) {
+            $('#nutella_version').text(atob(response.data.content));
+        }
+    });
 });
